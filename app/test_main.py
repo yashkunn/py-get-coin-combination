3e470223cca3fcfebf13a4d313727cb1329df51c
Yakiv Lobach <lobachyakiv@gmail.com>
2024-08-07 20:05:35 +0300

    first_try

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,30 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents,result",
+    [
+        pytest.param(
+            1, [1, 0, 0, 0],
+            id="Should return correct value for 1 cent"
+        ),
+        pytest.param(
+            6, [1, 1, 0, 0],
+            id="Should return correct value for 6 cents"
+        ),
+        pytest.param(
+            18, [3, 1, 1, 0],
+            id="Should return correct value for 18 cents"
+        ),
+        pytest.param(
+            50, [0, 0, 0, 2],
+            id="Should return correct value for 50 cents"
+        ),
+    ],
+)
+def test_should_return_correct_values(
+        cents: int, result: list[int]
+) -> None:
+    assert get_coin_combination(cents) == result
